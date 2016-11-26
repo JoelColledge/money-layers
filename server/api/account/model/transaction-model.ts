85ed34b14335c0ee3557c0710a8d4e78cc1142b7
Joel Colledge <joel.colledge@gmail.com>
2016-11-26 15:12:57 +0100

    Fix nested object schema

--- server/api/account/model/transaction-model.ts
+++ server/api/account/model/transaction-model.ts
@@ -2,13 +2,15 @@ import * as mongoose from 'mongoose';
 
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
+var entrySchema = new mongoose.Schema({
+    account: {type: ObjectId, required: true},
+    change: {type: Number, required: true}
+});
+
 var schema = new mongoose.Schema({
     description: {type: String, required: true, trim: true},
     date: {type: Date, required: true},
-    // entries: [
-    //     {account: ObjectId, required: true},
-    //     {change: Number, required: true}
-    // ],
+    entries: [entrySchema],
     createdAt: {type: Date, default: Date.now}
 });
 
