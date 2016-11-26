a5ec5001d64496089569b9ec26fbed7c0b2932a6
Joel Colledge <joel.colledge@gmail.com>
2016-11-26 11:09:14 +0100

    Bring in bootstrap 3

--- client/app-cmp.ts
+++ client/app-cmp.ts
@@ -3,12 +3,18 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   template: `
-    <h1 class="title">Money Layers Accounting</h1>
-    <nav>
-      <a routerLink="/todos" routerLinkActive="active">Todos</a>
-      <a routerLink="/accounts" routerLinkActive="active">Accounts</a>
-    </nav>
-    <router-outlet></router-outlet>
+    <div class="container-fluid">
+      <h1 class="title">Money Layers Accounting</h1>
+      <nav class="navbar navbar-default">
+        <ul class="nav navbar-nav">
+          <li><a routerLink="/todos" routerLinkActive="active">Todos</a></li>
+          <li><a routerLink="/accounts" routerLinkActive="active">Accounts</a></li>
+        </ul>
+      </nav>
+    </div>
+    <div class="container">
+      <router-outlet></router-outlet>
+    </div>
   `
 })
 export class AppCmp {
