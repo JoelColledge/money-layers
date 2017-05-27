101315c4b3d96fc835ff1d470f4b158489f1981d
Joel Colledge <joel.colledge@gmail.com>
2017-05-27 11:29:12 +0200

    Use the mLab Mongo URI to connect to the DB

--- server/config/db.conf.ts
+++ server/config/db.conf.ts
@@ -5,7 +5,7 @@ var dbConst = require('../constants/db.json');
 
 export class DBConfig {
     static init():void {
-      const URL = (process.env.NODE_ENV === 'production') ? process.env.MONGOHQ_URL
+      const URL = (process.env.NODE_ENV === 'production') ? process.env.MONGODB_URI
                                                           : dbConst.localhost;
 
       mongoose.connect(URL);
