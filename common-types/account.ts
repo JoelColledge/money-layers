ce17dd4ce1292cbb3a737c524a9443e1f70ced35
Joel Colledge <joel.colledge@gmail.com>
2017-05-27 14:43:04 +0200

    Remove layer from DB structure

--- common-types/account.ts
+++ common-types/account.ts
@@ -13,7 +13,6 @@ export class Account {
     constructor(
         public _id: any = undefined,
         public name: string = "",
-        public layer: string = "actual",
         public active: boolean = true,
         public groups: string[] = []
     ) { }
--- server/api/account/model/account-model.ts
+++ server/api/account/model/account-model.ts
@@ -2,7 +2,6 @@ import * as mongoose from 'mongoose';
 
 var schema = new mongoose.Schema({
     name: {type: String, required: true, trim: true},
-    layer: {type: String, required: true},
     active: {type: Boolean, required: true},
     createdAt: {type: Date, default: Date.now},
     groups: {type: [String], default: []}
