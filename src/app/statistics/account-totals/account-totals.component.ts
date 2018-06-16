39b74130cfebbfef121cdf3109502cf19560f40d
Joel Colledge <joel.colledge@gmail.com>
2018-06-16 11:23:52 +0200

    Make account totals grouped and pretty

--- src/app/statistics/account-totals/account-totals.component.ts
+++ src/app/statistics/account-totals/account-totals.component.ts
@@ -12,7 +12,7 @@ import {
 } from '../statistics.service';
 
 import {AccountTotal} from '../../../../common-types/statistics';
-import {Account} from '../../../../common-types/structure';
+import {Structure, Account} from '../../../../common-types/structure';
 
 @Component({
   selector: 'account-totals',
@@ -20,7 +20,7 @@ import {Account} from '../../../../common-types/structure';
   styleUrls: ['./account-totals.component.css']
 })
 export class AccountTotalsComponent implements OnInit {
-    accounts: Account[] = [];
+    accountsByTypes: {typeDisplayString: string, accounts: Account[]}[] = [];
     accountTotals: AccountTotal[] = [];
 
     constructor(
@@ -34,13 +34,27 @@ export class AccountTotalsComponent implements OnInit {
 
     private _getAll(): void {
         let structure = this.structureCacheService.get();
-        this.accounts = structure.accounts.filter(account => structure.accountTotalTypes.includes(account.type));
+        this.accountsByTypes = structure.accountTotalTypes
+            .map(typeName => ({
+                typeDisplayString: this._findTypeDisplayString(structure, typeName),
+                accounts: this._findAccountsByType(structure, typeName)
+            }));
+                structure.accounts.filter(account => structure.accountTotalTypes.includes(account.type));
 
         this._statisticsService
             .accountTotals()
             .subscribe((accountTotals) => {
                 this.accountTotals = accountTotals;
             });
+    }
+
+    private _findTypeDisplayString(structure: Structure, typeName: string): string {
+        let type = structure.types.find(type => type.name === typeName);
+        return type ? type.display : typeName;
+    }
+
+    private _findAccountsByType(structure: Structure, typeName: string): Account[] {
+        return structure.accounts.filter(account => account.type === typeName);
     }
 
     total(account: Account): number {
