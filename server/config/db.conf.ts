e2d1e879313e05b996ff41186b72288618afb18d
Joel Colledge <joel.colledge@gmail.com>
2019-11-30 17:55:38 +0100

    Fix mongoose deprecation warning about findOneAnd*

--- server/config/db.conf.ts
+++ server/config/db.conf.ts
@@ -8,6 +8,7 @@ export class DBConfig {
       const URL = (process.env.NODE_ENV === 'production') ? process.env.MONGODB_URI
                                                           : dbConst.localhost;
 
+      mongoose.set('useFindAndModify', false);
       mongoose.set('useUnifiedTopology', true);
       mongoose.connect(URL, { useNewUrlParser: true });
       mongoose.connection.on('error', console.error.bind(console, 'An error ocurred with the DB connection: '));
